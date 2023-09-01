5ba8399ac6cfe74b6f9df9d8d48b8480ce72c25b
NeoThePotato <neorayb@gmail.com>
2023-09-01 06:01:34 +0300

    Added empty Controllers for (Hopefully) all communication messages between client and server.

---
+++ Server/Controllers/ConnectToServerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ConnectToServerController : ControllerBase
+	{
+		// GET api/<SomethingController>/5s
+		[HttpGet("{id}")]
+		public void Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+ 
