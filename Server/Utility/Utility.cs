4e501bded3471d86b1958d465780277ab89f6dcc
NeoThePotato <neorayb@gmail.com>
2023-10-22 23:42:21 +0300

    Added Utilities.

---
+++ Server/Utility/Utility.cs
@@ -0,0 +1,32 @@
+﻿static class Utility
+{
+	public static float ClampMin(float val, float min)
+	{
+		return Math.Max(val, min);
+	}
+
+	public static float ClampMax(float val, float max)
+	{
+		return Math.Min(val, max);
+	}
+
+	public static float ClampRange(float val, float min, float max)
+	{
+		return ClampMin(ClampMax(val, max), min);
+	}
+
+	public static int ClampMin(int val, int min)
+	{
+		return Math.Max(val, min);
+	}
+
+	public static int ClampMax(int val, int max)
+	{
+		return Math.Min(val, max);
+	}
+
+	public static int ClampRange(int val, int min, int max)
+	{
+		return ClampMin(ClampMax(val, max), min);
+	}
+}
